7b66a83942182b6180a29187b4f8c199b072bf24
Alex <alexsalas0420@gmail.com>
2023-04-20 15:18:18 -0800

    sentencias
    Signed-off-by: Alex <alexsalas0420@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 
+//practicas sencillas Java
 
-import javax.swing.JOptionPane;
 public class Main {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
+           /* Uso de las sentencias if, else if
             String nombre = JOptionPane.showInputDialog("Escribe tu nombre:");
             String nacionalidad = JOptionPane.showInputDialog("Escribe tu nacionalidad");
             String altura = JOptionPane.showInputDialog("Escribe tu altura");
@@ -20,9 +21,79 @@ public class Main {
             JOptionPane.showMessageDialog(null, "Nombre: " + persona.getNombre()); //muestra el resultado del objetto y parametro
             JOptionPane.showMessageDialog(null, "Nacionalidad: " + persona.getNacionalidad());
             JOptionPane.showMessageDialog(null, "Altura: " + persona.getAltura());
-            JOptionPane.showMessageDialog(null, "Edad: " + persona.getEdad());
+            JOptionPane.showMessageDialog(null, "Edad: " + persona.getEdad());*/
 
+            /*Uso de las sentencias switch
+
+            String eleccion = JOptionPane.showInputDialog("selecciona un numero de mes");
+            int elec = Integer.parseInt(eleccion);
+
+            switch(elec){
+                case 1:
+                    JOptionPane.showMessageDialog(null,"Enero");
+                    break;
+                case 2:
+                    JOptionPane.showMessageDialog(null,"febrero");
+                    break;
+                case 3:
+                    JOptionPane.showMessageDialog(null,"Marzo");
+                    break;
+                case 4:
+                    JOptionPane.showMessageDialog(null,"Abril");
+                    break;
+                case 5:
+                    JOptionPane.showMessageDialog(null,"Mayo");
+                    break;
+                case 6:
+                    JOptionPane.showMessageDialog(null,"Junio");
+                    break;
+                case 7:
+                    JOptionPane.showMessageDialog(null,"Julio");
+                    break;
+                case 8:
+                    JOptionPane.showMessageDialog(null,"Agosto");
+                    break;
+                case 9:
+                    JOptionPane.showMessageDialog(null,"Septiembre");
+                    break;
+                case 10:
+                    JOptionPane.showMessageDialog(null,"Octubre");
+                    break;
+                case 11:
+                    JOptionPane.showMessageDialog(null,"Noviembre");
+                    break;
+                case 12:
+                    JOptionPane.showMessageDialog(null,"Diciembre  ");
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null,"Número de mes valido");
+            }
+        System.out.println("¿Hola, cuál es tu género?");
+        //nextLine se utiliza cuando intentamos leer cadenas de caracteres desde el teclado.
+        Scanner sc = new Scanner(System.in);
+        String genero = sc.nextLine();
+
+        switch (genero){
+            case "femenino" :
+            System.out.println("Eres mujer");
+        break;
+            case "masculino":
+            System.out.println("Eres hombre");
         }
-//no quiere ayuda porfavor
+
+        System.out.println("¿Cuál es tu edad?");
+        int edad = sc.nextInt();
+
+        if (edad > 18) {
+            System.out.println("Eres mayor de edad");
+        }
+        else if (edad < 18){
+            System.out.println("Eres menor de edad");
+        }
+
+        System.out.println("Hola,eres " + genero + " de " + edad + " años ");
+    */
+
     }
+}
 
